fbc2bc04771c3ff5d6885dfc2d047116b98f28cf
Himanshu Mohapatra <153461597+kinghim17@users.noreply.github.com>
2024-07-16 23:45:51 +0530

    Add files via upload

---
+++ Command/REGEXP.sql
@@ -0,0 +1,13 @@
+/*
+-- Query: SELECT *
+FROM customers 
+-- WHERE first_name REGEXP 'ELKA|ambur'
+-- WHERE last_name REGEXP 'EY|ON'
+-- WHERE last_name REGEXP '^MY| SE'
+WHERE last_name REGEXP 'B[RU]'
+LIMIT 0, 1000
+
+-- Date: 2024-07-16 01:50
+*/
+INSERT INTO `` (`customer_id`,`first_name`,`last_name`,`birth_date`,`phone`,`address`,`city`,`state`,`points`) VALUES (2,'Ines','Brushfield','1986-04-13','804-427-9456','14187 Commercial Trail','Hampton','VA',947);
+INSERT INTO `` (`customer_id`,`first_name`,`last_name`,`birth_date`,`phone`,`address`,`city`,`state`,`points`) VALUES (4,'Ambur','Roseburgh','1974-04-14','407-231-8017','30 Arapahoe Terrace','Orlando','FL',457);
