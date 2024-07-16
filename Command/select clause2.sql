7a556f00efbfcb14ae489b33b4c5af52f8c5bda9
Himanshu Mohapatra <153461597+kinghim17@users.noreply.github.com>
2024-07-16 23:46:47 +0530

    Add files via upload

---
+++ Command/select clause2.sql
@@ -0,0 +1,17 @@
+/*
+-- Query: SELECT name,unit_price,unit_price*1.10 AS new_price
+FROM products
+LIMIT 0, 1000
+
+-- Date: 2024-07-15 14:50
+*/
+INSERT INTO `` (`name`,`unit_price`,`new_price`) VALUES ('Foam Dinner Plate',1.21,1.3310);
+INSERT INTO `` (`name`,`unit_price`,`new_price`) VALUES ('Pork - Bacon,back Peameal',4.65,5.1150);
+INSERT INTO `` (`name`,`unit_price`,`new_price`) VALUES ('Lettuce - Romaine, Heart',3.35,3.6850);
+INSERT INTO `` (`name`,`unit_price`,`new_price`) VALUES ('Brocolinni - Gaylan, Chinese',4.53,4.9830);
+INSERT INTO `` (`name`,`unit_price`,`new_price`) VALUES ('Sauce - Ranch Dressing',1.63,1.7930);
+INSERT INTO `` (`name`,`unit_price`,`new_price`) VALUES ('Petit Baguette',2.39,2.6290);
+INSERT INTO `` (`name`,`unit_price`,`new_price`) VALUES ('Sweet Pea Sprouts',3.29,3.6190);
+INSERT INTO `` (`name`,`unit_price`,`new_price`) VALUES ('Island Oasis - Raspberry',0.74,0.8140);
+INSERT INTO `` (`name`,`unit_price`,`new_price`) VALUES ('Longan',2.26,2.4860);
+INSERT INTO `` (`name`,`unit_price`,`new_price`) VALUES ('Broom - Push',1.09,1.1990);
