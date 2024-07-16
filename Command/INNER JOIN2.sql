b6b5c3c434fef0fda59ad72129c0389788d8af19
Himanshu Mohapatra <153461597+kinghim17@users.noreply.github.com>
2024-07-16 23:36:29 +0530

    Add files via upload

---
+++ Command/INNER JOIN2.sql
@@ -0,0 +1,19 @@
+/*
+-- Query: SELECT order_id,o.customer_id,first_name,last_name
+FROM orders o
+JOIN customers c ON 
+   o.customer_id=c.customer_id
+LIMIT 0, 1000
+
+-- Date: 2024-07-16 06:30
+*/
+INSERT INTO `` (`order_id`,`customer_id`,`first_name`,`last_name`) VALUES (4,2,'Ines','Brushfield');
+INSERT INTO `` (`order_id`,`customer_id`,`first_name`,`last_name`) VALUES (7,2,'Ines','Brushfield');
+INSERT INTO `` (`order_id`,`customer_id`,`first_name`,`last_name`) VALUES (5,5,'Clemmie','Betchley');
+INSERT INTO `` (`order_id`,`customer_id`,`first_name`,`last_name`) VALUES (8,5,'Clemmie','Betchley');
+INSERT INTO `` (`order_id`,`customer_id`,`first_name`,`last_name`) VALUES (1,6,'Elka','Twiddell');
+INSERT INTO `` (`order_id`,`customer_id`,`first_name`,`last_name`) VALUES (10,6,'Elka','Twiddell');
+INSERT INTO `` (`order_id`,`customer_id`,`first_name`,`last_name`) VALUES (2,7,'Ilene','Dowson');
+INSERT INTO `` (`order_id`,`customer_id`,`first_name`,`last_name`) VALUES (3,8,'Thacher','Naseby');
+INSERT INTO `` (`order_id`,`customer_id`,`first_name`,`last_name`) VALUES (6,10,'Levy','Mynett');
+INSERT INTO `` (`order_id`,`customer_id`,`first_name`,`last_name`) VALUES (9,10,'Levy','Mynett');
