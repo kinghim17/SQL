afd9d9cae9582f7ef1c91f2a6c01b7cbc2451b82
Himanshu Mohapatra <153461597+kinghim17@users.noreply.github.com>
2024-07-16 23:34:22 +0530

    Add files via upload

---
+++ Command/implicit function.sql
@@ -0,0 +1,18 @@
+/*
+-- Query: SELECT*
+FROM orders o, customers c
+WHERE o.customer_id=c.customer_id
+LIMIT 0, 1000
+
+-- Date: 2024-07-16 18:30
+*/
+INSERT INTO `` (`order_id`,`customer_id`,`order_date`,`status`,`comments`,`shipped_date`,`shipper_id`,`customer_id`,`first_name`,`last_name`,`birth_date`,`phone`,`address`,`city`,`state`,`points`) VALUES (1,6,'2019-01-30',1,NULL,NULL,NULL,6,'Elka','Twiddell','1991-09-04','312-480-8498','7 Manley Drive','Chicago','IL',3073);
+INSERT INTO `` (`order_id`,`customer_id`,`order_date`,`status`,`comments`,`shipped_date`,`shipper_id`,`customer_id`,`first_name`,`last_name`,`birth_date`,`phone`,`address`,`city`,`state`,`points`) VALUES (2,7,'2018-08-02',2,NULL,'2018-08-03',4,7,'Ilene','Dowson','1964-08-30','615-641-4759','50 Lillian Crossing','Nashville','TN',1672);
+INSERT INTO `` (`order_id`,`customer_id`,`order_date`,`status`,`comments`,`shipped_date`,`shipper_id`,`customer_id`,`first_name`,`last_name`,`birth_date`,`phone`,`address`,`city`,`state`,`points`) VALUES (3,8,'2017-12-01',1,NULL,NULL,NULL,8,'Thacher','Naseby','1993-07-17','941-527-3977','538 Mosinee Center','Sarasota','FL',205);
+INSERT INTO `` (`order_id`,`customer_id`,`order_date`,`status`,`comments`,`shipped_date`,`shipper_id`,`customer_id`,`first_name`,`last_name`,`birth_date`,`phone`,`address`,`city`,`state`,`points`) VALUES (4,2,'2017-01-22',1,NULL,NULL,NULL,2,'Ines','Brushfield','1986-04-13','804-427-9456','14187 Commercial Trail','Hampton','VA',947);
+INSERT INTO `` (`order_id`,`customer_id`,`order_date`,`status`,`comments`,`shipped_date`,`shipper_id`,`customer_id`,`first_name`,`last_name`,`birth_date`,`phone`,`address`,`city`,`state`,`points`) VALUES (5,5,'2017-08-25',2,'','2017-08-26',3,5,'Clemmie','Betchley','1973-11-07',NULL,'5 Spohn Circle','Arlington','TX',3675);
+INSERT INTO `` (`order_id`,`customer_id`,`order_date`,`status`,`comments`,`shipped_date`,`shipper_id`,`customer_id`,`first_name`,`last_name`,`birth_date`,`phone`,`address`,`city`,`state`,`points`) VALUES (6,10,'2018-11-18',1,'Aliquam erat volutpat. In congue.',NULL,NULL,10,'Levy','Mynett','1969-10-13','404-246-3370','68 Lawn Avenue','Atlanta','GA',796);
+INSERT INTO `` (`order_id`,`customer_id`,`order_date`,`status`,`comments`,`shipped_date`,`shipper_id`,`customer_id`,`first_name`,`last_name`,`birth_date`,`phone`,`address`,`city`,`state`,`points`) VALUES (7,2,'2018-09-22',2,NULL,'2018-09-23',4,2,'Ines','Brushfield','1986-04-13','804-427-9456','14187 Commercial Trail','Hampton','VA',947);
+INSERT INTO `` (`order_id`,`customer_id`,`order_date`,`status`,`comments`,`shipped_date`,`shipper_id`,`customer_id`,`first_name`,`last_name`,`birth_date`,`phone`,`address`,`city`,`state`,`points`) VALUES (8,5,'2018-06-08',1,'Mauris enim leo, rhoncus sed, vestibulum sit amet, cursus id, turpis.',NULL,NULL,5,'Clemmie','Betchley','1973-11-07',NULL,'5 Spohn Circle','Arlington','TX',3675);
+INSERT INTO `` (`order_id`,`customer_id`,`order_date`,`status`,`comments`,`shipped_date`,`shipper_id`,`customer_id`,`first_name`,`last_name`,`birth_date`,`phone`,`address`,`city`,`state`,`points`) VALUES (9,10,'2017-07-05',2,'Nulla mollis molestie lorem. Quisque ut erat.','2017-07-06',1,10,'Levy','Mynett','1969-10-13','404-246-3370','68 Lawn Avenue','Atlanta','GA',796);
+INSERT INTO `` (`order_id`,`customer_id`,`order_date`,`status`,`comments`,`shipped_date`,`shipper_id`,`customer_id`,`first_name`,`last_name`,`birth_date`,`phone`,`address`,`city`,`state`,`points`) VALUES (10,6,'2018-04-22',2,NULL,'2018-04-23',2,6,'Elka','Twiddell','1991-09-04','312-480-8498','7 Manley Drive','Chicago','IL',3073);
