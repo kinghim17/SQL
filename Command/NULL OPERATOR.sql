0fb1374114225632d1fee307ab1a48a7b345d27a
Himanshu Mohapatra <153461597+kinghim17@users.noreply.github.com>
2024-07-16 23:40:30 +0530

    Add files via upload

---
+++ Command/NULL OPERATOR.sql
@@ -0,0 +1,13 @@
+/*
+-- Query: SELECT *
+FROM orders
+WHERE shipped_date IS NULL
+LIMIT 0, 1000
+
+-- Date: 2024-07-16 01:57
+*/
+INSERT INTO `` (`order_id`,`customer_id`,`order_date`,`status`,`comments`,`shipped_date`,`shipper_id`) VALUES (1,6,'2019-01-30',1,NULL,NULL,NULL);
+INSERT INTO `` (`order_id`,`customer_id`,`order_date`,`status`,`comments`,`shipped_date`,`shipper_id`) VALUES (3,8,'2017-12-01',1,NULL,NULL,NULL);
+INSERT INTO `` (`order_id`,`customer_id`,`order_date`,`status`,`comments`,`shipped_date`,`shipper_id`) VALUES (4,2,'2017-01-22',1,NULL,NULL,NULL);
+INSERT INTO `` (`order_id`,`customer_id`,`order_date`,`status`,`comments`,`shipped_date`,`shipper_id`) VALUES (6,10,'2018-11-18',1,'Aliquam erat volutpat. In congue.',NULL,NULL);
+INSERT INTO `` (`order_id`,`customer_id`,`order_date`,`status`,`comments`,`shipped_date`,`shipper_id`) VALUES (8,5,'2018-06-08',1,'Mauris enim leo, rhoncus sed, vestibulum sit amet, cursus id, turpis.',NULL,NULL);
