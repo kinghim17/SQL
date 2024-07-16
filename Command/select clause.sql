2c46fda5b7a813a7a762c7da89e9e9d2204b3ed2
Himanshu Mohapatra <153461597+kinghim17@users.noreply.github.com>
2024-07-16 23:46:18 +0530

    Add files via upload

---
+++ Command/select clause.sql
@@ -0,0 +1,21 @@
+/*
+-- Query: SELECT 
+last_name,
+first_name,
+points,
+points*10+10 AS discount_factor
+FROM customers
+LIMIT 0, 1000
+
+-- Date: 2024-07-15 12:06
+*/
+INSERT INTO `` (`last_name`,`first_name`,`points`,`discount_factor`) VALUES ('MacCaffrey','Babara',2273,22740);
+INSERT INTO `` (`last_name`,`first_name`,`points`,`discount_factor`) VALUES ('Brushfield','Ines',947,9480);
+INSERT INTO `` (`last_name`,`first_name`,`points`,`discount_factor`) VALUES ('Boagey','Freddi',2967,29680);
+INSERT INTO `` (`last_name`,`first_name`,`points`,`discount_factor`) VALUES ('Roseburgh','Ambur',457,4580);
+INSERT INTO `` (`last_name`,`first_name`,`points`,`discount_factor`) VALUES ('Betchley','Clemmie',3675,36760);
+INSERT INTO `` (`last_name`,`first_name`,`points`,`discount_factor`) VALUES ('Twiddell','Elka',3073,30740);
+INSERT INTO `` (`last_name`,`first_name`,`points`,`discount_factor`) VALUES ('Dowson','Ilene',1672,16730);
+INSERT INTO `` (`last_name`,`first_name`,`points`,`discount_factor`) VALUES ('Naseby','Thacher',205,2060);
+INSERT INTO `` (`last_name`,`first_name`,`points`,`discount_factor`) VALUES ('Rumgay','Romola',1486,14870);
+INSERT INTO `` (`last_name`,`first_name`,`points`,`discount_factor`) VALUES ('Mynett','Levy',796,7970);
