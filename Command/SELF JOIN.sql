c115807208913a7ed92358fd16b3ace2821ea380
Himanshu Mohapatra <153461597+kinghim17@users.noreply.github.com>
2024-07-16 23:48:15 +0530

    Add files via upload

---
+++ Command/SELF JOIN.sql
@@ -0,0 +1,31 @@
+/*
+-- Query: SELECT 
+      e.employee_id,
+      e.first_name,
+      m.first_name AS manager
+FROM employees e
+JOIN employees m
+     ON e.reports_to = m.employee_id
+LIMIT 0, 1000
+
+-- Date: 2024-07-16 16:56
+*/
+INSERT INTO `` (`employee_id`,`first_name`,`manager`) VALUES (33391,'D\'arcy','Yovonnda');
+INSERT INTO `` (`employee_id`,`first_name`,`manager`) VALUES (37851,'Sayer','Yovonnda');
+INSERT INTO `` (`employee_id`,`first_name`,`manager`) VALUES (40448,'Mindy','Yovonnda');
+INSERT INTO `` (`employee_id`,`first_name`,`manager`) VALUES (56274,'Keriann','Yovonnda');
+INSERT INTO `` (`employee_id`,`first_name`,`manager`) VALUES (63196,'Alaster','Yovonnda');
+INSERT INTO `` (`employee_id`,`first_name`,`manager`) VALUES (67009,'North','Yovonnda');
+INSERT INTO `` (`employee_id`,`first_name`,`manager`) VALUES (67370,'Elladine','Yovonnda');
+INSERT INTO `` (`employee_id`,`first_name`,`manager`) VALUES (68249,'Nisse','Yovonnda');
+INSERT INTO `` (`employee_id`,`first_name`,`manager`) VALUES (72540,'Guthrey','Yovonnda');
+INSERT INTO `` (`employee_id`,`first_name`,`manager`) VALUES (72913,'Kass','Yovonnda');
+INSERT INTO `` (`employee_id`,`first_name`,`manager`) VALUES (75900,'Virge','Yovonnda');
+INSERT INTO `` (`employee_id`,`first_name`,`manager`) VALUES (76196,'Mirilla','Yovonnda');
+INSERT INTO `` (`employee_id`,`first_name`,`manager`) VALUES (80529,'Lynde','Yovonnda');
+INSERT INTO `` (`employee_id`,`first_name`,`manager`) VALUES (80679,'Mildrid','Yovonnda');
+INSERT INTO `` (`employee_id`,`first_name`,`manager`) VALUES (84791,'Hazel','Yovonnda');
+INSERT INTO `` (`employee_id`,`first_name`,`manager`) VALUES (95213,'Cole','Yovonnda');
+INSERT INTO `` (`employee_id`,`first_name`,`manager`) VALUES (96513,'Theresa','Yovonnda');
+INSERT INTO `` (`employee_id`,`first_name`,`manager`) VALUES (98374,'Estrellita','Yovonnda');
+INSERT INTO `` (`employee_id`,`first_name`,`manager`) VALUES (115357,'Ivy','Yovonnda');
