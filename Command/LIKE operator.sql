7ed224e7d210b1825194b8914b8368fa96cf2854
Himanshu Mohapatra <153461597+kinghim17@users.noreply.github.com>
2024-07-16 23:38:53 +0530

    Add files via upload

---
+++ Command/LIKE operator.sql
@@ -0,0 +1,11 @@
+/*
+-- Query: SELECT*
+FROM customers
+WHERE last_name LIKE 'b%'
+LIMIT 0, 1000
+
+-- Date: 2024-07-16 01:28
+*/
+INSERT INTO `` (`customer_id`,`first_name`,`last_name`,`birth_date`,`phone`,`address`,`city`,`state`,`points`) VALUES (2,'Ines','Brushfield','1986-04-13','804-427-9456','14187 Commercial Trail','Hampton','VA',947);
+INSERT INTO `` (`customer_id`,`first_name`,`last_name`,`birth_date`,`phone`,`address`,`city`,`state`,`points`) VALUES (3,'Freddi','Boagey','1985-02-07','719-724-7869','251 Springs Junction','Colorado Springs','CO',2967);
+INSERT INTO `` (`customer_id`,`first_name`,`last_name`,`birth_date`,`phone`,`address`,`city`,`state`,`points`) VALUES (5,'Clemmie','Betchley','1973-11-07',NULL,'5 Spohn Circle','Arlington','TX',3675);
