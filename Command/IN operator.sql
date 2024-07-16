5590636b969d5fca42424dc16a2bab0f7fdf5e6c
Himanshu Mohapatra <153461597+kinghim17@users.noreply.github.com>
2024-07-16 23:34:37 +0530

    Add files via upload

---
+++ Command/IN operator.sql
@@ -0,0 +1,10 @@
+/*
+-- Query: SELECT *
+FROM products
+WHERE quantity_in_stock IN('49','38','72')
+LIMIT 0, 1000
+
+-- Date: 2024-07-15 18:04
+*/
+INSERT INTO `` (`product_id`,`name`,`quantity_in_stock`,`unit_price`) VALUES (2,'Pork - Bacon,back Peameal',49,4.65);
+INSERT INTO `` (`product_id`,`name`,`quantity_in_stock`,`unit_price`) VALUES (3,'Lettuce - Romaine, Heart',38,3.35);
