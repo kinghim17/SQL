de69e9e323f0a7c969ed4f6f6dcf98e4e43e31ff
Himanshu Mohapatra <153461597+kinghim17@users.noreply.github.com>
2024-07-16 23:49:21 +0530

    Add files via upload

---
+++ Command/where clause.sql
@@ -0,0 +1,9 @@
+/*
+-- Query: SELECT*
+FROM orders
+WHERE order_date > '2019-01-01'
+LIMIT 0, 1000
+
+-- Date: 2024-07-15 15:04
+*/
+INSERT INTO `` (`order_id`,`customer_id`,`order_date`,`status`,`comments`,`shipped_date`,`shipper_id`) VALUES (1,6,'2019-01-30',1,NULL,NULL,NULL);
