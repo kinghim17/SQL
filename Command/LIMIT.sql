dd97b17a125589d3064c3ab3058e6f14c870287a
Himanshu Mohapatra <153461597+kinghim17@users.noreply.github.com>
2024-07-16 23:39:07 +0530

    Add files via upload

---
+++ Command/LIMIT.sql
@@ -0,0 +1,10 @@
+/*
+-- Query: SELECT *
+FROM customers
+ORDER BY points DESC
+LIMIT 3
+-- Date: 2024-07-16 06:17
+*/
+INSERT INTO `` (`customer_id`,`first_name`,`last_name`,`birth_date`,`phone`,`address`,`city`,`state`,`points`) VALUES (5,'Clemmie','Betchley','1973-11-07',NULL,'5 Spohn Circle','Arlington','TX',3675);
+INSERT INTO `` (`customer_id`,`first_name`,`last_name`,`birth_date`,`phone`,`address`,`city`,`state`,`points`) VALUES (6,'Elka','Twiddell','1991-09-04','312-480-8498','7 Manley Drive','Chicago','IL',3073);
+INSERT INTO `` (`customer_id`,`first_name`,`last_name`,`birth_date`,`phone`,`address`,`city`,`state`,`points`) VALUES (3,'Freddi','Boagey','1985-02-07','719-724-7869','251 Springs Junction','Colorado Springs','CO',2967);
