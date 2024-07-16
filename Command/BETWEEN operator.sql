ff092061861123a3d3fa49076b01500ecb488e11
Himanshu Mohapatra <153461597+kinghim17@users.noreply.github.com>
2024-07-16 23:33:36 +0530

    Add files via upload

---
+++ Command/BETWEEN operator.sql
@@ -0,0 +1,11 @@
+/*
+-- Query: SELECT *
+FROM customers
+WHERE birth_date BETWEEN '1990/01/01' AND '2000/01/01'
+LIMIT 0, 1000
+
+-- Date: 2024-07-15 18:09
+*/
+INSERT INTO `` (`customer_id`,`first_name`,`last_name`,`birth_date`,`phone`,`address`,`city`,`state`,`points`) VALUES (6,'Elka','Twiddell','1991-09-04','312-480-8498','7 Manley Drive','Chicago','IL',3073);
+INSERT INTO `` (`customer_id`,`first_name`,`last_name`,`birth_date`,`phone`,`address`,`city`,`state`,`points`) VALUES (8,'Thacher','Naseby','1993-07-17','941-527-3977','538 Mosinee Center','Sarasota','FL',205);
+INSERT INTO `` (`customer_id`,`first_name`,`last_name`,`birth_date`,`phone`,`address`,`city`,`state`,`points`) VALUES (9,'Romola','Rumgay','1992-05-23','559-181-3744','3520 Ohio Trail','Visalia','CA',1486);
